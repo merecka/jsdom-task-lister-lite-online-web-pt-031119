456f44ff97dc686d5d4fac3b914063e76768fb04
Alex Merecka <merecka@gmail.com>
2019-10-09 10:00:30 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ document.addEventListener("DOMContentLoaded", () => {
   form_variable.addEventListener("submit", function(e){
    		e.preventDefault() //prevents form from being submitted
   		const new_task = document.createElement('li')
-  		new_task.innerText = task_desc_field.value
-  		tasks_list.appendChild(new_task)
-  		let delete_button = document.createElement("BUTTON")
-  		delete_button.innerHTML = "Delete"
-  		new_task.appendChild(delete_button)
+  		new_task.innerText = task_desc_field.value // sets the individual task string equal to what is written in the description field
+  		tasks_list.appendChild(new_task) // appends individual tasks to the UL "tasks"
+  		let delete_button = document.createElement("BUTTON") // creates a delete button
+  		delete_button.innerHTML = "Delete" // adds the word Delete to the delete button
+  		new_task.appendChild(delete_button) // adds delete buttont to each task
 
   		function removeItem() {
   			tasks_list.removeChild(new_task) // removes individual tasks from tasks list
